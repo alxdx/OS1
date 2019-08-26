f6658038006693f5f6df029555e7dac81f87a3b2
alxdx <ustongs09@gmail.com>
2019-08-26 13:35:54 -0500

    ejercicio 1 fork()

---
+++ dia4/eg1Fork.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+	pid_t pid;
+	int file1;
+	if((pid=fork())==0){
+		creat("datos.dat",0777);
+		file1=open("datos.dat",01);
+		for(int i=1;i<=10;i++){
+			write(file1,&i,sizeof(int));
+		}
+	}else{
+		int n;
+		file1=open("datos.dat",00);
+		read(file1,&n,sizeof(int));
+		printf("%d\n",n );
+	}
+
+	return 0;
+}
+
+
