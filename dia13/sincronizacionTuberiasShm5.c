28d0b1286f324db7c73d3b3652818a56cb58c829
alxdx <ustongs09@gmail.com>
2019-11-11 14:33:17 -0600

    ejercicios de sincronizacion con pipes y shm

--- dia13/sincronizacionTuberiasShm5.c
+++ dia13/sincronizacionTuberiasShm5.c
@@ -4,7 +4,9 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <sys/shm.h>
-#define MAX 100
+#define MAX 2000
+#define PASO 5
+
 void generarPares(int memoria, int t1, int t2,int t3);
 void generarImpares(int memoria, int t1, int t2,int t3);
 void consumirNumeros(int memoria,int t3);
@@ -15,7 +17,7 @@ int main(int argc, char const *argv[]){
 	int t1[2], t2[2],t3[2],pid1,pid2;
 	int shmId;
 	key_t key=ftok(".",'a');
-	shmId=shmget(key,sizeof(int)*2,0666|IPC_CREAT);
+	shmId=shmget(key,sizeof(int)*(PASO*2),0666|IPC_CREAT);
 	
 	if (pipe(t1)<0){
 		perror("no se puede crear la tuberia: ");	
@@ -76,18 +78,27 @@ void generarPares(int shmId, int t1, int t2,int t3){
 	char testigo;
 	int *ptr;
 	ptr=shmat(shmId,NULL,0);
-	*ptr=i;
-	printf("->%d\n",*ptr);
+  int k=0;
+  for (i = 0; i <(PASO*2) ;i+=2){
+		*(ptr+k)=i;
+	  //printf("->%d\n",*(ptr+k));
+    k++;
+	}
   //cede el turno a p2
   write(t3,&testigo,sizeof(char));
   write(t1,&testigo,sizeof(char));
-	for (int i = 2; i < MAX; i=i+2){
+  
+	for (int i = (PASO*2); i < MAX; i=i+(PASO*2)){
 		//espera turno
 		read(t2,&testigo,sizeof(char));
 		//inserta numero a memoria
-			*ptr=i;
+    int h=0;
+    for (int j = i; j <(i+(PASO*2)) ;j+=2){
+			*(ptr+h)=j;
+		  //printf("->%d\n",*(ptr+h));
+      h++;
+		}
 		write(t3,&testigo,sizeof(char));
-		//printf("->%d\n",*ptr);
     //cede turno
 		write(t1,&testigo,sizeof(char));
 	}
@@ -101,13 +112,17 @@ void generarImpares(int shmId, int t1, int t2,int t3){
 	char testigo;
 	int *ptr;
 	ptr=shmat(shmId,NULL,0);
-	for (i = 1; i < MAX; i=i+2){
+	for (i = 1; i < MAX; i=i+(PASO*2)){
 		//espera turno
 		read(t1,&testigo,sizeof(char));
 		//inserta numero a memoria
-		*(ptr+1)=i;
+		int h=PASO;
+    for (int j = i; j <(i+(PASO*2)) ;j+=2){
+			*(ptr+h)=j;
+		  //printf("->%d\n",*(ptr+h));
+      h++;
+		}
 		write(t3,&testigo,sizeof(char));
-    //printf("->%d -- %d\n",*ptr,*(ptr+1));
 		//cede turno
 		write(t2,&testigo,sizeof(char));
 	}
@@ -121,13 +136,21 @@ void consumirNumeros(int shmId,int t3){
 	char testigo;
 	i=shmat(shmId,NULL,0);
 	int j=0;
-	while(j<MAX){
+	while(j<(MAX/PASO)){
 		read(t3,&testigo,sizeof(char));
 		if(band){
-      printf("%d\n", *i);
+      int h=0;
+      for (int k = 0; k <PASO ;k+=1){
+        printf("%d\n", *(i+h));
+        h++;
+      }
       band=0;
     }else{
-      printf("%d\n", *(i+1));
+      int h=PASO;
+      for (int k = 0; k <PASO ;k+=1){
+        printf("%d\n", *(i+h));
+        h++;
+      }
       band=1;      
     }
 	  j+=1;
@@ -136,3 +159,4 @@ void consumirNumeros(int shmId,int t3){
 	shmctl(shmId,IPC_RMID,NULL);
 	return;
 }
+  
